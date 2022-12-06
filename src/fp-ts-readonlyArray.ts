df5ae8cc72ea93e6ed589dd245ca35f4c52f6184
welentsai <welentsai@gmail.com>
2022-12-06 21:26:52 +0800

    add readonlyarray traverse example

--- src/fp-ts-readonlyArray.ts
+++ src/fp-ts-readonlyArray.ts
@@ -1,5 +1,6 @@
 import * as RA from 'fp-ts/ReadonlyArray'
 import * as O from 'fp-ts/Option'
+import * as E from 'fp-ts/Either'
 import { pipe } from 'fp-ts/function'
 
 const a = O.of('a')
@@ -22,3 +23,22 @@ result = pipe(
     RA.sequence(O.Applicative)
 )
 console.log(result)
+
+
+// traverse example
+
+const parseInteger = (s: string): E.Either<string, number> => {
+    const n = parseInt(s, 10)
+    return isNaN(n) ? E.left(`${s} is not a number`) : E.right(n)
+}
+
+console.log(parseInteger('1'))
+console.log(parseInteger('2'))
+console.log(parseInteger('3'))
+
+const E1 = E.of('1')
+const E2 = E.of('2')
+const E3 = E.of('3')
+
+const result2 = RA.traverse(E.Applicative)(parseInteger)(['1', '2', '3'])
+console.log(result2)
