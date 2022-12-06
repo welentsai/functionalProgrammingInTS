b82b091a4f5cc83a71c8302f6adb91e6a89c55ba
welentsai <welentsai@gmail.com>
2022-12-06 23:03:01 +0800

    parallel validation with applicative

--- src/parallel-applicative.ts
+++ src/parallel-applicative.ts
@@ -1,8 +1,13 @@
+import * as assert from 'assert'
 import { pipe, flow } from "fp-ts/function";
 import * as E from "fp-ts/Either"
 import * as T from 'fp-ts/Task'
 import * as TE from "fp-ts/TaskEither"
 import { sequenceS, sequenceT } from "fp-ts/Apply";
+import * as RA from 'fp-ts/ReadonlyArray'
+import * as S from 'fp-ts/Semigroup'
+import * as string from 'fp-ts/string'
+import * as A from 'fp-ts/Apply'
 
 interface AppConfig {
     readonly host: string; // web-server host name
@@ -12,8 +17,8 @@ interface AppConfig {
 
 const validateHost = (host: string): E.Either<Error, string> => {
     console.log(Date.now(), "-validateHost-")
-    // return E.right(host)
-    return E.left(new Error("host not valid !!"))
+    return E.right(host)
+    // return E.left(new Error("host not valid !!"))
 }
 
 const validatePort = (port: number): E.Either<Error, number> => {
@@ -28,8 +33,26 @@ const validateConnectionString = (connectionString: string): E.Either<Error, str
     return E.right(connectionString)
 }
 
+const validateHost2 = (host: string): E.Either<string[], string> => {
+    console.log(Date.now(), "-validateHost-")
+    // return E.right(host)
+    return E.left(["host not valid !!"])
+}
+
+const validatePort2 = (port: number): E.Either<string[], number> => {
+    console.log(Date.now(), "-validatePort--")
+    // return E.right(port)
+    return E.left(["port not valid !!"])
+}
+
+const validateConnectionString2 = (connectionString: string): E.Either<string[], string> => {
+    console.log(Date.now(), "-validateConnectionString-")
+    T.delay(1000)
+    return E.right(connectionString)
+}
+
 const data: AppConfig = {
-    host: "http://123",
+    host: "http://123.tsmc.com",
     port: 5566,
     connectionString: "http://abcd"
 }
@@ -69,7 +92,7 @@ const apConfig = pipe(
 console.log(apConfig)
 
 
-// sequence
+// sequence validation !!
 const result4 = pipe(
     data,
     ({ host, port, connectionString }) =>
@@ -83,4 +106,41 @@ const result4 = pipe(
 console.log(result4)
 
 
+const validateAppConfig = (input: AppConfig): E.Either<Error, AppConfig> =>
+    pipe(
+        E.Do,
+        E.apS('host', validateHost(input.host)),
+        E.apS('port', validatePort(input.port)),
+        E.apS('connectionString', validateConnectionString(input.connectionString))
+    )
+
+assert.deepStrictEqual(validateAppConfig(data), E.left(new Error("port not valid !!"))) // <= first error
+
+
+// 取得所有的 Errors , 用 Semigroup 連起來
+// if you want to get all errors you need to provide a way to concatenate them via a Semigroup
+// const Applicative = E.getApplicativeValidation(pipe(string.Semigroup, S.intercalate(', ')))
+const Applicative = E.getApplicativeValidation(RA.getSemigroup<string>())
+const apS = A.apS(Applicative)
+
+
+// get all errors
+const validateAppConfig2 = (input: AppConfig): E.Either<readonly string[], AppConfig> =>
+    pipe(
+        E.Do,
+        apS('host', validateHost2(input.host)),
+        apS('port', validatePort2(input.port)),
+        apS('connectionString', validateConnectionString2(input.connectionString))
+    )
+
+
+console.log('validateAppConfig2', validateAppConfig2(data))
+
+pipe(
+    data,
+    validateAppConfig2,
+    E.match(
+        (e) => console.log(e),
+        (a) => console.log(a))
+)
 
