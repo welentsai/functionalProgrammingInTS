cb9e4994e457f2fc1dd0e9e16db9728614ff06b9
welentsai <welentsai@gmail.com>
2023-08-20 21:15:17 +0800

    chore: refactor test description

--- test/effect_schema/validationError.test.ts
+++ test/effect_schema/validationError.test.ts
@@ -3,8 +3,8 @@ import { NotFoundError } from '../../src/effect_schema/validationError'
 import * as E from 'fp-ts/Either'
 
 // The two tests marked with concurrent will be run in parallel
-describe('validation test suite', () => {
-    it('NotFoundError test', async () => {
+describe('NotFoundError test suite', () => {
+    it('given message\'s length less than 50, should have a right NotFoundError', async () => {
         const notFoundError = { _tag: 'NotFoundError', message: 'resource not found' }
         const result = NotFoundError.of('resource not found')
         assert.deepStrictEqual(result, E.right(notFoundError))
