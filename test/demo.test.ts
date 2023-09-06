8a64ad0e3eeb37e18d8a04ea15838846a82d4fe7
welentsai <welentsai@gmail.com>
2023-09-06 23:56:41 +0800

    test: regular expression test

--- test/demo.test.ts
+++ test/demo.test.ts
@@ -1,5 +1,21 @@
 import { expect, test } from 'vitest'
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3)
+// test('adds 1 + 2 to equal 3', () => {
+//     expect(1 + 2).toBe(3)
+// })
+
+test('regualr expreesion test', () => {
+    // const reg = /^@.*\/$/
+    // const reg = /@[A-Z\u4E00-\u9FFF]*\/\w+/gi
+    const reg = /@[A-Z\u4E00-\u9FFF ]*\/\w+/gi
+
+    // const reg = /@.*\/\w+/gi
+
+    const msg = '@YB @蔡維倫/wltsai @abc kab koruak/korua plese help to comment @詩婷/stli'
+
+    const result = msg.match(reg)
+
+    console.log(result)
+
+    console.log('result', JSON.stringify(result))
 })
